2413a09902285cbce42b6175990445ca8cf46716
chrisjoonlee <christopherjoon@gmail.com>
2022-12-11 16:19:36 +0900

    App now has a landing page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 import { useEffect } from 'react';
+import { Route, Routes, useSearchParams } from 'react-router-dom'
+
+import LandingPage from './components/LandingPage'
+import './App.css';
 
 function App() {
   const client_id = "23a3c6f9357c415085bd245ca334cfab";
-  const redirect_uri = "localhost:3000/"
+  const redirect_uri = "http://127.0.0.1:3000/"
   const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}`
 
+  let [searchParams, setSearchParams] = useSearchParams();
+
   useEffect(() => {
-    window.location.href = url
+    // window.location.href = url
   }, []);
 
   return (
     <div>
-      <h1>Hey baby</h1>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/main" />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LandingPage = () => {
+    console.log("yas")
+
+    return (
+        <>
+            <h1>Spotify Playlist Pitcher</h1>
+            <button>Access the Spotify API</button>
+        </>
+    )
+};
+
+export default LandingPage;
