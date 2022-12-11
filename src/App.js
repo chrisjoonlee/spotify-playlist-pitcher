b5baf9e19721d47d45a1962e086416ec9d5aa6e7
chrisjoonlee <christopherjoon@gmail.com>
2022-12-11 16:33:39 +0900

    Page now responds to whether 'code' appears in the url params

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ import LandingPage from './components/LandingPage'
 import './App.css';
 
 function App() {
-  const client_id = "23a3c6f9357c415085bd245ca334cfab";
-  const redirect_uri = "http://127.0.0.1:3000/"
-  const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}`
-
-  let [searchParams, setSearchParams] = useSearchParams();
-
-  useEffect(() => {
-    // window.location.href = url
-  }, []);
-
   return (
     <div>
       <Routes>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const LandingPage = () => {
-    console.log("yas")
+    const client_id = "23a3c6f9357c415085bd245ca334cfab";
+    const redirect_uri = "http://127.0.0.1:3000/";
+    const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}`;
 
-    return (
-        <>
-            <h1>Spotify Playlist Pitcher</h1>
-            <button>Access the Spotify API</button>
-        </>
-    )
+    let [searchParams, setSearchParams] = useSearchParams();
+
+    // Landing page
+    if (!searchParams.get("code")) {
+        return (
+            <>
+                <h1>Spotify Playlist Pitcher</h1>
+                <button onClick={() => {
+                    console.log("ouch")
+                    window.location.replace(url);
+                }}>
+                    Access the Spotify API
+                </button>
+            </>
+        )
+    }
+    // If "code" appears in the url params
+    else {
+        return (
+            <>next stop</>
+        )
+    }
 };
 
 export default LandingPage;
