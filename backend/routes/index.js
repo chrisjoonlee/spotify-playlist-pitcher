aa4f9cc6ecb6367e3025df644ced8765655d20e6
chrisjoonlee <christopherjoon@gmail.com>
2022-12-11 17:57:38 +0900

    Successfully accessed Spotify's auth token API

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const fetch = (...args) =>
+  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const router = express.Router();
+
+// Define variables for Spotify API access
+const client_id = "23a3c6f9357c415085bd245ca334cfab";
+const client_secret = "aa5e3e7a7313476dbe60910e78022075";
+const auth_str = "Authorization: Basic " +
+  Buffer.from(client_id + ":" + client_secret).toString('base64');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.send('Slurp that dick till it cum!');
+});
+
+router.get('/code', async function (req, res) {
+  // Receive code from front-end
+  const code = req.query.code;
+  console.log("Code", code);
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Authorization": auth_str
+    },
+    body: JSON.stringify({
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: "http://271.0.0.1/"
+    })
+  });
+  console.log("RESPONSE", response)
 });
 
 module.exports = router;
--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -2,52 +2,17 @@ import React, { useEffect } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 const LandingPage = () => {
+    // Define variables for Spotify API access
     const client_id = "23a3c6f9357c415085bd245ca334cfab";
-    const client_secret = "aa5e3e7a7313476dbe60910e78022075";
-    const redirect_uri = "http://127.0.0.1:3000/";
-    const base_url = "https://accounts.spotify.com";
-    const url = `${base_url}/authorize?client_id=${client_id}&response_type=code&redirect_uri=${redirect_uri}`;
+    const url = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://127.0.0.1:3000/`;
 
     let [searchParams, setSearchParams] = useSearchParams();
 
     useEffect(() => {
-        const auth_str = "Authorization: Basic " +
-            Buffer.from(client_id + ":" + client_secret).toString('base64');
-
+        // After the user accepts Spotify's API usage
         if (searchParams.get("code")) {
-            console.log("url:", `${base_url}/api/token`);
-            console.log("auth_str:", auth_str);
-            console.log("code:", searchParams.get("code"));
-
-            const request = new Request(`${base_url}/api/token`, {
-                method: "POST",
-                headers: new Headers({
-                    "Content-Type": "application/x-www-form-urlencoded",
-                    "Authorization": auth_str
-                }),
-                body: JSON.stringify({
-                    grant_type: "authorization_code",
-                    code: searchParams.get("code"),
-                    redirect_uri
-                })
-            });
-
-            console.log("REQUEST:", request);
-
-            fetch(request).then(response => console.log("RESPONSE:", response));
-
-            // fetch(`${base_url}/api/token`, {
-            //     method: "POST",
-            //     headers: {
-            //         "Content-Type": "application/x-www-form-urlencoded",
-            //         "Authorization": auth_str
-            //     },
-            //     body: JSON.stringify({
-            //         grant_type: "authorization_code",
-            //         code: searchParams.get("code"),
-            //         redirect_uri
-            //     })
-            // }).then(response => console.log("RESPONSE:", response));
+            // Call to Express server to fetch Spotify's auth token
+            const response = fetch("http://localhost:3001/code?code=" + searchParams.get("code"));
         }
     }, [searchParams])
 
@@ -57,7 +22,6 @@ const LandingPage = () => {
             <>
                 <h1>Spotify Playlist Pitcher</h1>
                 <button onClick={() => {
-                    console.log("ouch")
                     window.location.replace(url);
                 }}>
                     Access the Spotify API
@@ -68,7 +32,7 @@ const LandingPage = () => {
     // If "code" appears in the url params
     else {
         return (
-            <>next stop</>
+            <>hey baby</>
         )
     }
 };
